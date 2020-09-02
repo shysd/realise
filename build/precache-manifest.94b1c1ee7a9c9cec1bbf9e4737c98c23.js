cc124bf2d67d1547cfd446659dc40df01c91a9ef
shreyasdethe <shreyasdethe9@gmail.com>
2020-09-02 23:00:48 +0530

    Initial commit

---
+++ build/precache-manifest.94b1c1ee7a9c9cec1bbf9e4737c98c23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3347f6cb6c42be69aa7c2c83182bc9f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9418139d40d3d24da7ac",
+    "url": "/static/css/main.5cf83141.chunk.css"
+  },
+  {
+    "revision": "f51b2fbad035f2995fd9",
+    "url": "/static/js/2.8d102227.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d102227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9418139d40d3d24da7ac",
+    "url": "/static/js/main.7768d26c.chunk.js"
+  },
+  {
+    "revision": "c350b3a622c20fed229c",
+    "url": "/static/js/runtime-main.07cbc8da.js"
+  }
+]);
